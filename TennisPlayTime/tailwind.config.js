e818f2401ba525b4e1d272a6be9d2c2933142cdb
Alfio Puglisi <puglisi_algio@ciao.it>
2024-10-14 13:24:28 +0200

    importazione colori personalizzati in tailwind.config.js

--- TennisPlayTime/tailwind.config.js
+++ TennisPlayTime/tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lime: {
+          300: '#A4D65E',
+        },
+        gray: {
+          800: '#363636',
+          400: '#B3B3B3',
+        },
+        mint: '#3EB489',
+        yellow: '#D2E000',
+        night: '#2A2D34',
+        orange: '#FFA500',
+      },
+    },
   },
   plugins: [],
 }
